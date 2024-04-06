678072b5ab7528f91daf487e5141c8eaa77e9bc0
harikka <harikkasai@gmail.com>
2024-04-06 19:46:15 +0530

    final

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const path = require('path');
 
 const parserService=require('./parser');
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
