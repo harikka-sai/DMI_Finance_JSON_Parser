d3169c3efbf4a3c0e83d3127ab03d6d3d7e3601b
harikka <harikkasai@gmail.com>
2024-04-06 19:07:18 +0530

    changed added

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const bodyParser = require('body-parser');
-
 const parserService=require('./parser');
-
-app.use(bodyParser.json());
-
-app.post('/parseJson', async (req, res) => {
-    try {
-        // Convert JSON to CSV
-        await parserService.main(req.body);
-        // Create CSV file
-        const csvFilePath = './output.csv';
-        // Return CSV file as a response
-        res.download(csvFilePath, 'output.csv');
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
+    parserService.main();
 });
