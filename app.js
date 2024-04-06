ec1aff5faab81bf702b886e649ce063f55e71cdf
harikka <harikkasai@gmail.com>
2024-04-06 19:30:36 +0530

    latest changes

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
 const parserService=require('./parser');
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/parseJson', async (req, res) => {
+    try {
+        const jsonData = req.body.data;
+
+        if (!jsonData || typeof jsonData !== 'string') {
+            throw new Error('Invalid JSON data');
+        }
+
+        await parserService.main(jsonData);
+
+        const csvFilePath = './output.csv';
+        res.download(csvFilePath, 'output.csv');
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    parserService.main();
+    console.log(`Server is running on port ${PORT}`);
 });
